3253d5443868fd88fae068808846399934a1b78b
Aleksey Eltsov <eltsov_aleksey@forecsys.ru>
2021-05-03 22:44:06 +0300

    Basket.API

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; }
+        public List<ShoppingCartItem> Items { get; set; } = new();
+
+        public ShoppingCart()
+        {
+        }
+
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+        }
+
+        public decimal TotalPrice 
+        { 
+            get
+            {
+                decimal totalPrice = 0;
+                Items.ForEach(a => totalPrice += a.Price * a.Quantity);
+
+                return totalPrice;
+            }
+        }
+    }
+}
